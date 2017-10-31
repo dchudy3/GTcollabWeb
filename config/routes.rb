b4613415f30aea4cac33f05310c982ec45f8d339
Natalie (ナタリー） <castro.natalie94@gmail.com>
2017-10-30 22:32:54 -0400

    More UI changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do 
 	get '/login', to: 'login#verify_login'
 
+	get '/meetings/new'
+	get '/groups/new'
+
 	root 'login#login'
 	resources :groups
 	resources :meetings
